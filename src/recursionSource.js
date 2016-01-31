227825ddcadc9de4ba078a731cd243583a968e40
Will Wheeler <willfulbard@gmail.com>
2016-01-31 11:36:18 -0800

    Get all prompts working

--- src/recursionSource.js
+++ src/recursionSource.js
@@ -7,7 +7,16 @@
 // NOTE: to pass the "must use recursion" check in the test environment, 
 // you must use recursion on the classicFibonacci function itself. Thus, inner function recursion won't trigger it. 
 var classicFibonacci = function(endNumber) {
-	//Your code here
+  if (endNumber === 0) {
+    return 0
+  }
+
+  if (endNumber <= 1) {
+    return 1
+  }
+
+  return classicFibonacci(endNumber - 2) + classicFibonacci(endNumber - 1);
+
 };
 
 // Takes a starting number and multiplies it by one less than itself until 0. 
@@ -15,8 +24,11 @@ var classicFibonacci = function(endNumber) {
 //
 // NOTE: to pass the "must use recursion" check in the test environment, 
 // you must use recursion on the factorial function itself. Thus, inner function recursion won't trigger it. 
-var factorial = function(num){
-	//Your code here
+var factorial = function(num) {
+  if (num < 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Takes a multidimensional array and converts it to a one-dimensional array.
@@ -24,7 +36,19 @@ var factorial = function(num){
 //
 // Hint: Use Array.isArray to check if something is an array
 var flatten = function(array){
-	//Your code here
+  var result = [];
+
+  var flattenRecurse = function(array) {
+    for (var i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i]) === true) {
+        flattenRecurse(array[i]);
+      } else {
+        result.push(array[i]);
+      }
+    }
+  };
+  flattenRecurse(array);
+  return result;
 };
 
 // Takes DOM to be checked and the tag you are looking to count as parameters. 
@@ -32,21 +56,76 @@ var flatten = function(array){
 //
 // BONUS if it defaults to document.body if there is only one argument. 
 var tagCount = function(givenDOM, tagName) {
-	//Your code here
+  var count = arguments[2];
+  if (arguments[2] === undefined) {
+    count = 0;
+  }
+
+  if (arguments.length === 1) {
+    tagName = arguments[0];
+    givenDOM = document.body;
+  }
+  
+  if (givenDOM.nodeType !== Node.ELEMENT_NODE) {
+    return count;
+  }
+
+  if (givenDOM.nodeName === tagName) {
+    count++;
+  }
+
+  var childNodes = givenDOM.childNodes;
+  for (var prop in childNodes) {
+    count = tagCount(childNodes[prop], tagName, count);
+  }
+  return count;
 };
 
 // Takes a ordered list of integers as its argument. 
 // Should start from the middle and move outwards. 
 // For inspiration: http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBinarySearch.html
-var searchBinary = function(orderedList, item){
-	//Your code here
+var searchBinary = function(orderedList, item) {
+  if (orderedList.length === 0) {
+    return false;
+  }
+
+  var middle = Math.floor(orderedList.length / 2);
+  var middleItem = orderedList[middle];
+
+  if (item === middleItem) {
+    return true;
+  }
+  
+  if (orderedList.length === 1) {
+    return false;
+  }
+
+  if (item < middleItem) {
+    return searchBinary(orderedList.slice(0,middle));
+  } else {
+    return searchBinary(orderedList.slice(middle+1));
+  }
+
 };
+
 
 // Additional problems for consideration
 
 // Same as factorial, but instead of returning the n!,
 // it returns the array of factorial numbers up to (and including) n! 
 // NOTE: One may remove the result argument, and write in a private closure function instead.
-var factorialArray = function(n, result) {
-	//Your code here
+var factorialArray = function(num) {
+  if (num < 0) {
+    return undefined;
+  }
+  var result = [];
+  var _recurseFactorial = function (num) {
+    if (num >= 0) {
+      result.unshift(factorial(num));
+      _recurseFactorial(num - 1);
+    }
+  };
+
+  _recurseFactorial(num);
+  return result;
 };
